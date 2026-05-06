125e2aab95d127653b5b312619bf34050eab92f1
Thomas Lemaire <lemairetomtom@gmail.com>
2026-05-06 12:54:02 +0200

    refonte du design des formulaires d inscription et de connexion

--- vues/inscription.php
+++ vues/inscription.php
@@ -2,38 +2,67 @@
 $titre = "Site e-commerce 2022-2023 : Inscription";
 ob_start()
 ?>
-<h1 class="text-center">Inscription</h1>
-<form action="#" method="POST">
-    <div class="mb-3">
-    <label for="nom" class="form-label">Nom</label>
-    <input type="text" class="form-control" id="nom" name="nom">
+
+<div class="container d-flex justify-content-center align-items-center mt-5 mb-5">
+    <div class="col-md-6 col-lg-5">
+
+        <div class="card shadow">
+            <div class="card-body p-4">
+
+                <h2 class="text-center mb-4">Créer un compte</h2>
+
+                <form action="#" method="POST">
+
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label for="nom" class="form-label">Nom</label>
+                            <input type="text" class="form-control" id="nom" name="nom" required>
+                        </div>
+
+                        <div class="col-md-6 mb-3">
+                            <label for="prenom" class="form-label">Prénom</label>
+                            <input type="text" class="form-control" id="prenom" name="prenom" required>
+                        </div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="pseudo" class="form-label">Pseudo</label>
+                        <input type="text" class="form-control" id="pseudo" name="pseudo" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="tel" class="form-label">Téléphone</label>
+                        <input type="text" class="form-control" id="tel" name="tel">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="date_naissance" class="form-label">Date de naissance</label>
+                        <input type="date" class="form-control" id="date_naissance" name="date_naissance">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Adresse email</label>
+                        <input type="email" class="form-control" id="email" name="email" required>
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="motdepasse" class="form-label">Mot de passe</label>
+                        <input type="password" class="form-control" id="motdepasse" name="motdepasse" required>
+                    </div>
+
+                    <div class="d-grid">
+                        <button type="submit" class="btn btn-primary">
+                            S'inscrire
+                        </button>
+                    </div>
+
+                </form>
+
+            </div>
+        </div>
+
     </div>
-    <div class="mb-3">
-    <label for="prenom" class="form-label">Prenom</label>
-    <input type="text" class="form-control" id="prenom" name="prenom">
-    </div>
-    <div class="mb-3">
-    <label for="pseudo" class="form-label">Pseudo</label>
-    <input type="text" class="form-control" id="pseudo" name="pseudo">
-    </div>
-    <div class="mb-3">
-    <label for="tel" class="form-label">Telephone</label>
-    <input type="text" class="form-control" id="tel" name="tel">
-    </div>
-    <div class="mb-3">
-    <label for="date_naissance" class="form-label">Date de Naissance</label>
-    <input type="date" class="form-control" id="date_naissance" name="date_naissance">
-    </div>
-    <div class="mb-3">
-    <label for="email" class="form-label">Email</label>
-    <input type="email" class="form-control" id="email" name="email">
-    </div>
-    <div class="mb-3">
-    <label for="motdepasse" class="form-label">Mot de passe</label>
-    <input type="password" class="form-control" id="motdepasse" name="motdepasse">
-    </div>
-    <button type="submit" class="btn btn-primary">S'inscrire</button>
-</form>
+</div>
 
 <?php
 $contenu = ob_get_clean();
